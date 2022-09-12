584d7a170bec01d4b084fb6c9fbf2e607419b601
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2022-09-12 11:44:39 +0700

    Add files via upload

---
+++ Rini puspita Wati pretest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	
+	string a;
+	string b;
+	string c;
+	cout<<"Masukan Nama : ";
+	getline(cin,a);
+	cout<<"Masukan Umur : ";
+	grtline(cin,b);
+	cout<<"Masukan Harapan : ";
+	getline(cin,c);
+	cout<<"Nama saya adalah"<<a;
+	cout<<"\tdengan Umur"<<b;
+	cout<<"\tharapan saya adalah"<<c;
+	
+	return 0;
+}
